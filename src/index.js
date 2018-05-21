0a4355485a96784576d7a44a2553aa5101e6b5c5
Sathors <sathors@free.fr>
2018-05-20 22:16:50 -0500

    Connection to mongo is working.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import 'babel-polyfill'
+import mongoose from 'mongoose'
+
+const openConnection = () => new Promise((resolve, reject) => {
+  mongoose.connect('mongodb://localhost/bookshelf')
+  const db = mongoose.connection
+  db.on('error', () => {
+    reject(new Error('connection error'))
+  })
+  db.once('open', () => {
+    console.log('connected!')
+    resolve()
+  })
+})
 
 const main = async () => {
-  console.log('coucou')
+  await openConnection()
 }
 
 main()
