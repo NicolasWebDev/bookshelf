cc4fdc95b9e9942fbd5d7b8556767575f913390b
Sathors <sathors@free.fr>
2018-05-24 11:31:27 -0500

    Now the db has been migrated and it works.

--- spec/Book.spec.js
+++ spec/Book.spec.js
@@ -1,38 +1,79 @@
+import _ from 'lodash'
 import Book from '../src/Book'
 
+expect.extend({
+  toHaveSameDataThan (received, argument) {
+    const pass = _.isEqual(_.omit(received.toObject(), '_id'),
+      _.omit(argument.toObject(), '_id'))
+    const message = pass
+      ? () => `expected ${received} to have the same data than ${argument}`
+      : () => `expected ${received} to not have the same data than ${argument}`
+    return { pass, message }
+  }
+})
+
 describe('Book', () => {
-  describe('toText', () => {
-    let book
+  let book1, text1, book2, text2
 
-    beforeEach(() => {
-      book = new Book({
-        title: 'Les Misérables',
-        author: 'Victor Hugo',
-        stars: 4.5,
-        nbReviews: 1655,
-        observation: 'One of my favourites books',
-        tags: ['novel', 'french']
-      })
+  beforeEach(() => {
+    book1 = new Book({
+      title: 'Les Misérables',
+      author: 'Victor Hugo',
+      stars: 4.5,
+      nbReviews: 1655,
+      observation: 'One of my favourites books',
+      tags: ['novel', 'french']
     })
+    text1 = 'Les Misérables, by Victor Hugo *4.5/1655 One of my favourites' +
+      ' books +novel +french'
+    book2 = new Book({
+      title: 'Harry Potter',
+      author: 'J.K. Rowling',
+      stars: 4,
+      nbReviews: 100,
+      tags: ['novel', 'english']
+    })
+    text2 = 'Harry Potter, by J.K. Rowling *4/100 +novel +english'
+  })
 
+  describe('toText', () => {
     it('returns a textual representation of the book', () => {
-      expect(book.toText())
-        .toBe('Les Misérables, by Victor Hugo *4.5/1655 One of my favourites' +
-          ' books +novel +french')
+      expect(book1.toText()).toBe(text1)
     })
 
     it('shows the priority at the beginning', () => {
-      book.priority = 'A'
-      expect(book.toText())
-        .toBe('(A) Les Misérables, by Victor Hugo *4.5/1655' +
-          ' One of my favourites books +novel +french')
+      book1.priority = 'A'
+      expect(book1.toText()).toBe(`(A) ${text1}`)
     })
 
     it('shows if the book is downloaded', () => {
-      book.downloaded = true
-      expect(book.toText())
-        .toBe('. Les Misérables, by Victor Hugo *4.5/1655' +
-          ' One of my favourites books +novel +french')
+      book1.downloaded = true
+      expect(book1.toText()).toBe(`. ${text1}`)
+    })
+
+    it('does not output a trailing space if there are no tags', () => {
+      book2.tags = []
+      expect(book2.toText()).toBe('Harry Potter, by J.K. Rowling *4/100')
+    })
+  })
+
+  describe('fromText', () => {
+    it('creates a book from a text line', () => {
+      expect(Book.fromText(text1)).toHaveSameDataThan(book1)
+    })
+
+    it('creates a book from another text line', () => {
+      expect(Book.fromText(text2)).toHaveSameDataThan(book2)
+    })
+
+    it('creates a book from another text line', () => {
+      book2.priority = 'A'
+      expect(Book.fromText(`(A) ${text2}`)).toHaveSameDataThan(book2)
+    })
+
+    it('checks if the book has been downloaded', () => {
+      book2.downloaded = true
+      expect(Book.fromText(`. ${text2}`)).toHaveSameDataThan(book2)
     })
   })
 })
--- src/Book.js
+++ src/Book.js
@@ -17,14 +17,28 @@ bookSchema.methods = {
     return `${this.priority ? `(${this.priority}) ` : ''}` +
       `${this.downloaded ? '. ' : ''}${this.title}, by ${this.author}` +
       ` *${this.stars}/${this.nbReviews}` +
-      ` ${this.observation ? `${this.observation} ` : ''}` +
-      `${this.tags.filter(tag => tag).map(tag => `+${tag}`).join(' ')}`
+      `${this.observation ? ` ${this.observation}` : ''}` +
+      `${this.tags.length ? ' ' + this.tags.filter(tag => tag).map(tag => `+${tag}`).join(' ') : ''}`
   }
 }
 
 bookSchema.statics = {
   toText: async function () {
     return (await this.find()).map(book => book.toText()).join('\n')
+  },
+  fromText: function (text) {
+    const matches = text.match(/^(?:\((\w)\) )?(?:(\.) )?(.+), by (.+) \*(\d\.?\d?)\/(\d+)(.*?)((?: \+\w+)*)$/)
+    try {
+      let [priority, downloaded, title, author, stars, nbReviews, observation, tags] = matches.slice(1)
+      tags = tags.split(' +').filter(elt => elt)
+      const instance = new this({ title, author, stars, nbReviews, tags })
+      if (observation) instance.observation = observation.slice(1)
+      if (priority) instance.priority = priority
+      if (downloaded) instance.downloaded = true
+      return instance
+    } catch (e) {
+      console.log(text)
+    }
   }
 }
 
