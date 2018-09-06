06e6ee1476a99d76156b1ca997d1ca942b5604e7
Sathors <sathors@free.fr>
2018-09-06 17:34:19 -0500

    Work with bodyParser.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill'
 import express from 'express'
+import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import Book from './Book'
 
@@ -7,6 +8,7 @@ const DB_URL = 'mongodb://localhost/bookshelf'
 const PORT = 3001
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
 
 mongoose.connect(DB_URL)
 mongoose.Promise = global.Promise
@@ -16,6 +18,11 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 app.get('/', async (req, res) => {
   const books = await Book.find()
   res.send({ books })
+})
+
+app.post('/book', (req, res) => {
+  console.log(req.body)
+  res.json(req.body)
 })
 
 app.listen(PORT, () => {
