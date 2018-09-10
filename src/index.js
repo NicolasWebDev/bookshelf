1f089ebe2c44b7ecc59f5dba6467149537f0b94e
Sathors <sathors@free.fr>
2018-09-10 11:09:03 -0500

    Add error-handling.

--- src/index.js
+++ src/index.js
@@ -3,28 +3,39 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import Book from './Book'
+import asyncHandler from 'express-async-handler'
 
 const DB_URL = 'mongodb://localhost/bookshelf'
 const PORT = 3001
 
-const app = express()
-app.use(bodyParser.json())
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send({ error: err.message })
+}
 
 mongoose.connect(DB_URL)
 mongoose.Promise = global.Promise
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
+const app = express()
+app.use(bodyParser.json())
+
 app.get('/', async (req, res) => {
   const books = await Book.find()
   res.send({ books })
 })
 
-app.post('/book', async (req, res) => {
-  const { amazonURL } = req.body
-  const book = await Book.fromAmazonURL(amazonURL)
-  res.json(await book.save())
-})
+app.post(
+  '/book',
+  asyncHandler(async (req, res) => {
+    const { amazonURL } = req.body
+    const book = await Book.fromAmazonURL(amazonURL)
+    res.json(await book.save())
+  })
+)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`)
